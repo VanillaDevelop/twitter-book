59176c9a04729e0273c267ec93f6964bac72274e
Vanilla <vanilla@nya.gg>
2023-08-10 02:23:27 +0200

    Draft for tweet and data profile typing and interface showing user setup is needed

--- src/components/DataProfile/DataProfile.tsx
+++ src/components/DataProfile/DataProfile.tsx
@@ -4,6 +4,21 @@ import { DataProfileType } from "@/types"
 
 export default function DataProfile(props: {user?: DataProfileType, children?: ReactNode, small: boolean, moving: string, animationCallback: () => void})
 {
+    let userData = null
+    if(props.user && !(props.user.has_tweets && props.user.has_contexts))
+    {
+        userData = (
+                <p>
+                    This profile is not fully set up and requires further steps before it can be turned into a book.
+                    <ul>
+                        <li>Data {props.user.has_tweets ? "" : "NOT"} Analyzed</li>
+                        <li>Context {props.user.has_contexts ? "" : "NOT"} Scraped</li>
+                    </ul>
+                    
+                </p>
+        )
+    }
+
     return (
         <div className={`profileCard ${props.small ? "small " : ""}${props.moving}`} onAnimationEnd={props.animationCallback}>
             <div className="profileHeader">
@@ -11,7 +26,8 @@ export default function DataProfile(props: {user?: DataProfileType, children?: R
                 {props.user ? <h4>{props.user.twitter_handle}</h4> : <h4>Add New User</h4>}
             </div>
             <div className="profileContent">
-                {props.children}
+                {props.user && userData}
+                {!props.user && props.children}
             </div>
         </div>
     )
--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,6 @@ export interface DataProfileType
     uuid: string;
     twitter_handle: string;
     has_tweets: boolean,
-    tweets: TweetType[],
     has_contexts: boolean
 }
 
@@ -13,11 +12,37 @@ export interface DataProfileContextType
     setDataProfiles: (profiles: DataProfileType[]) => void;
 }
 
+export enum MediaType {
+    Photo = "PHOTO",
+    Video = "VIDEO",
+    Gif = "GIF"
+}
+
+export interface TweetMediaType
+{
+    internal_url: string;
+    type: MediaType
+}
+
+export interface ReplyElement
+{
+    original_author_id: string;
+    original_author_display_name: string;
+    original_tweet_text: string;
+    original_tweet_created_at: string;
+    reply_media?: TweetMediaType[];
+}
+
 export interface TweetType 
 {
     id: string;
     text: string;
-    created_at: string;
+    created_at: Date;
+    context_collected: boolean;
+    reply_chain?: ReplyElement[];
+    direct_rt_data?: ReplyElement;
+    qrt_data?: ReplyElement;
+    media?: TweetMediaType[];
 }
 
 export interface PopUpType
