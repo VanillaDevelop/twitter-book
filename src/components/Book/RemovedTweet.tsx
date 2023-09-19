585ebf06e9ae477982e9af7feeaf20446b5520ca
Vanilla <vanilla@nya.gg>
2023-09-19 02:01:24 +0200

    Styling for removed tweets

--- src/components/Book/RemovedTweet.tsx
+++ src/components/Book/RemovedTweet.tsx
@@ -1,10 +1,23 @@
 import { TweetRelation } from "@/types";
+import "./DisplayTweet.scss"
 
 export default function RemovedTweet(props: {prev_relation: TweetRelation})
 {
     return (
-        <div>
-            Removed Tweet Placeholder
+        <div className="tweet">
+            <div className="tweet_main">
+                <div className="tweet_sidepanel">
+                    <img src="public/images/unknownuser.png" className="profileImage"/>
+                    <div className="tweet_chain"></div>
+                </div>
+                <div className="tweet_content">
+                    <div className="tweet_date">Unknown</div>
+                    <div className="tweet_author">Unknown</div>
+                    <div className="tweet_text">
+                        This tweet has been deleted.
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
